24f660bfe0ff1c10b3b61ffc7202796e5b310c74
SJ.Z <522617302@qq.com>
2018-10-07 17:16:29 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/cose/coolweather/WeatherActivity.java
+++ app/src/main/java/com/cose/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.cose.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.cose.coolweather.gson.Forecast;
 import com.cose.coolweather.gson.Weather;
+import com.cose.coolweather.service.AutoUpdateService;
 import com.cose.coolweather.util.HttpUtil;
 import com.cose.coolweather.util.Utility;
 
@@ -223,5 +225,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
